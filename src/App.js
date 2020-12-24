195158174ca8815a2eec6bfa4459d5a71c0935b9
jennyzhang7 <zyjenny31@gmail.com>
2020-12-23 23:31:14 -0500

    update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
 import {
   TextField,
-  Button,
   Layout,
   Page,
   Icon,
@@ -9,9 +8,20 @@ import {
   List,
   Banner,
   Spinner,
+  Button,
 } from "@shopify/polaris";
+// import Button from "@material-ui/core/Button";
 import { SearchMinor } from "@shopify/polaris-icons";
 import Confetti from "react-confetti";
+import Paper from "@material-ui/core/Paper";
+import Grid from "@material-ui/core/Grid";
+import { makeStyles } from "@material-ui/core/styles";
+import MaterialUICard from "@material-ui/core/Card";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import Typography from "@material-ui/core/Typography";
 
 export default function App() {
   const url = new URL(window.location.href);
@@ -27,7 +37,7 @@ export default function App() {
     if (searchValue) {
       fetchMovies(searchValue);
     }
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   const fetchMovies = (movieName) => {
@@ -56,8 +66,8 @@ export default function App() {
   };
 
   const nominateMovie = (movie) => {
-    const { Title, Year, imdbID } = movie;
-    const newNominations = [...nominations, { Title, Year, imdbID }];
+    const { Title, Year, imdbID, Poster } = movie;
+    const newNominations = [...nominations, { Title, Year, imdbID, Poster }];
     setNominations(newNominations);
     localStorage.setItem("nominations", JSON.stringify(newNominations));
   };
@@ -77,6 +87,17 @@ export default function App() {
     );
   };
 
+  const useStyles = makeStyles((theme) => ({
+    root: {
+      flexGrow: 1,
+    },
+    paper: {
+      padding: theme.spacing(2),
+      textAlign: "center",
+      color: theme.palette.text.secondary,
+    },
+  }));
+  const classes = useStyles();
   return (
     <Page title="The Shoppies">
       <Layout>
@@ -86,8 +107,42 @@ export default function App() {
         >
           {nominations.length >= 5 && (
             <>
-              <Confetti />
+              {/* <Confetti /> */}
               <Banner title="YAY YOU HAVE 5 NOMINATIONS" status="success" />
+              <div className={classes.root}>
+                <Grid container spacing={3}>
+                  {nominations.map((nomination, idx) => (
+                    <Grid item xs>
+                      <MaterialUICard className={classes.root}>
+                        <CardActionArea>
+                          <CardMedia
+                            component="img"
+                            height="140"
+                            width="140"
+                            image={nomination.Poster}
+                          />
+                          <CardContent>
+                            <Typography
+                              gutterBottom
+                              variant="h5"
+                              component="h2"
+                            >
+                              {nomination.Title}
+                            </Typography>
+                            <Typography
+                              variant="body2"
+                              color="textSecondary"
+                              component="p"
+                            >
+                              {nomination.Year}
+                            </Typography>
+                          </CardContent>
+                        </CardActionArea>
+                      </MaterialUICard>
+                    </Grid>
+                  ))}
+                </Grid>
+              </div>
             </>
           )}
         </Layout.Section>
@@ -116,33 +171,39 @@ export default function App() {
                 : "Please search for a movie title and press 'Enter'"
             }
           >
-            <Card.Section>
-              {loading ? (
-                <Spinner
-                  accessibilityLabel="Spinner example"
-                  size="large"
-                  color="teal"
-                />
-              ) : (
-                <List type="bullet">
-                  {results.map((movie) => (
-                    <List.Item key={movie.imdbID}>
-                      {movie.Title} ({movie.Year}){" "}
-                      <Button
-                        size="slim"
-                        disabled={shouldBeDisabled(movie)}
-                        onClick={() => nominateMovie(movie)}
-                      >
-                        Nominate
-                      </Button>
-                    </List.Item>
-                  ))}
-                  {searchTitle.length > 0 &&
-                    results.length === 0 &&
-                    "No results found."}
-                </List>
-              )}
-            </Card.Section>
+            <div className="listItem">
+              <Card.Section>
+                {loading ? (
+                  <Spinner
+                    accessibilityLabel="Spinner example"
+                    size="large"
+                    color="teal"
+                  />
+                ) : (
+                  <List type="bullet">
+                    {results.map((movie) => (
+                      <List.Item key={movie.imdbID}>
+                        {movie.Title} ({movie.Year}){" "}
+                        <Button
+                          size="slim"
+                          disabled={shouldBeDisabled(movie)}
+                          onClick={() => nominateMovie(movie)}
+                        >
+                          Nominate
+                        </Button>
+                        {/* <Button variant="contained" color="primary">
+                          Nominate
+                        </Button> */}
+                      </List.Item>
+                    ))}
+
+                    {searchTitle.length > 0 &&
+                      results.length === 0 &&
+                      "No results found."}
+                  </List>
+                )}
+              </Card.Section>
+            </div>
           </Card>
         </Layout.Section>
         <Layout.Section oneHalf>
